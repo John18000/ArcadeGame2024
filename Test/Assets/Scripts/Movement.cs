9c6b764aaec2639414713d2ed6c5892f0f2da1b8
John18000 <Johnnie.alt14@gmail.com>
2024-01-18 10:36:22 -0500

    New Input System
    -All movement converted, done except for jumping tweaks

--- Test/Assets/Scripts/Movement.cs
+++ Test/Assets/Scripts/Movement.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
+
+    private Vector2 movementInput = Vector2.zero;
+    private bool jumped = false;
+    private bool isFacingRight = true;
+    
     //base movement
     public float speed;
     private float moveHorizontal;
@@ -33,21 +39,13 @@ public class Movement : MonoBehaviour
 
     void Update()
     {
+        //Modifies isGrounded variable
         GroundCheck();
 
         moveHorizontal = Input.GetAxis("Horizontal");
 
         //jump input
-        if(Input.GetKeyDown(KeyCode.W))
-        {
-            if (isGrounded || jumpCount <= maxJumps)
-            {
-                jump = true;
-                isGrounded = false;
-                jumpCount++;
-            }
-            
-        }
+       
 
         //dash input
         if(Input.GetKeyDown(KeyCode.Space) && canDash)
@@ -66,22 +64,40 @@ public class Movement : MonoBehaviour
             rb.velocity = new Vector2(moveHorizontal * speed, rb.velocity.y);
         }
 
-        if (jump)
+        /*if (jump)
         {
             Jump();
-        }
+        }*/
 
         if (dash)
         {
             StartCoroutine(Dash()); 
         }
+
+        if (!isFacingRight && moveHorizontal > 0f)
+        {
+            Flip();
+        }
+        else if (isFacingRight && moveHorizontal < 0f)
+        {
+            Flip();
+        }
     }
 
-    void Jump()
+    public void Jump(InputAction.CallbackContext context)
     {
-        rb.velocity = new Vector2(rb.velocity.x, 0);
-        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-        jump = false;
+        if (context.performed && isGrounded && jumpCount > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            jumpCount--;
+        }
+
+        if (context.canceled && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
     }
 
     IEnumerator Dash()
@@ -114,6 +130,25 @@ public class Movement : MonoBehaviour
         if(isGrounded) {
             jumpCount = maxJumps;
         }
+
+        if(!isGrounded)
+        {
+            jumpCount = maxJumps - 1;
+        }
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveHorizontal = context.ReadValue<Vector2>().x;
     }
 
     void OnCollisionEnter2D(Collision2D col)
